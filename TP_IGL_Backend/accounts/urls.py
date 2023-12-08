cafdd9b97da47aa1344bc8fd9a11708a4fcb63f3
Salah <ls_nacef@esi.dz>
2023-12-09 00:01:50 +0100

    JWT added

--- TP_IGL_Backend/accounts/urls.py
+++ TP_IGL_Backend/accounts/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
-from .views import register
+from .views import register, MyTokenObtainPairView
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+)
 
 urlpatterns = [
   path('register', register),
+  path('login', MyTokenObtainPairView.as_view()),
+  path('token/refresh', TokenRefreshView.as_view()),
 ]
--- TP_IGL_Backend/accounts/views.py
+++ TP_IGL_Backend/accounts/views.py
@@ -4,6 +4,19 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['name'] = user.name
+        token['role'] = user.role
+        return token
+    
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
