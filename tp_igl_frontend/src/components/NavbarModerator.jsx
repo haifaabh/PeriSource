a4a4f5a17110021b52b99f9dee6baf28d0ad3296
Kamelia ham <111534083+kameliaham@users.noreply.github.com>
2024-02-05 20:22:23 +0100

    Update NavbarModerator.jsx

--- tp_igl_frontend/src/components/NavbarModerator.jsx
+++ tp_igl_frontend/src/components/NavbarModerator.jsx
@@ -4,9 +4,11 @@ import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
 import NameLogo from '../assets/NameLogo.svg'
 import logo from '../assets/logo.svg'
 import ExitToAppOutlinedIcon from '@mui/icons-material/ExitToAppOutlined';
+import { useAuth } from '../AuthContext'
+
 
 export const NavbarModerator = () => {
-
+    const { setAuthenticatedUser } = useAuth(); 
     const [nav,setnav] = useState(true) ;
 
     const handlenav = () => {
@@ -20,19 +22,21 @@ export const NavbarModerator = () => {
       setSelectedItem(item);
     };
 
+    const handleSignOut = () => {
+        setAuthenticatedUser(null);
+        window.location.href = '/signin';
+      };
+
   return (
     <div className='bg-white flex justify-between items-center h-24 w-screen mx-auto  text-[#4D4D4D] border-b-1.5 border-[#ABBED1] border-opacity-50 lg:px-24 px-4'>
        <div className={!nav ? 'hidden' : 'flex justify-between items-center '}>
-            {/* <div className='lg:block hidden'>
-                <img src={logo} alt="" className="w-full h-auto" />
-            </div> */}
             <div className='mx-2 '>
                 <img src={NameLogo} alt="PeriSource" className="w-full h-auto" />
             </div>
         </div>
        
         <div className="hidden md:flex lg:ml-4">
-            <button className="lg:px-2 px-0 py-2 text-sm lg:text-base text-[#002366] font-montserrat font-semibold border border-[#002366] hover:bg-[#e0ebf6]">
+            <button onClick={handleSignOut} className="lg:px-2 px-0 py-2 text-sm lg:text-base text-[#002366] font-montserrat font-semibold border border-[#002366] hover:bg-[#e0ebf6]">
                 Sign Out   
                 <ExitToAppOutlinedIcon className='lg:ml-2 text-[#002366]'/>
             </button>
@@ -51,8 +55,7 @@ export const NavbarModerator = () => {
                 <li className='p-4'><a  href="#Home" onClick={() => handlenav()} className="p-4  border-b border-b-gray-600 text-sm lg:text-base hover:text-gray-400 font-montserrat font-semibold">Home</a></li>
                 <li className='p-4'><a  href="#About" onClick={() => handlenav()} className="p-4  border-b border-b-gray-600 text-sm lg:text-base hover:text-gray-400 font-montserrat font-semibold">About Us</a></li>
                 <li className='p-4'><a href="#Contact" onClick={() => handlenav()} className=" p-4  border-b border-b-gray-600 text-sm lg:text-base hover:text-gray-400 font-montserrat font-semibold">Contact Us</a></li>
-                <li className='p-4'><a href="" onClick={() => handlenav()} className="p-4  border-b border-b-gray-600 text-sm lg:text-base hover:text-gray-400 font-montserrat font-semibold">Favoris
-                </a></li>
+                <li className='p-4'><a href="" onClick={() => handleSignOut()} className=" p-4  border-b border-b-gray-600 text-sm lg:text-base hover:text-gray-400 font-montserrat font-semibold">Sign out</a></li>
             </ul>    
         </div>
         </div>
