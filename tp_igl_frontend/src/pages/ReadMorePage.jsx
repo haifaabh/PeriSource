51cba833402592052bace93696eb581138e93a0a
Kamelia ham <111534083+kameliaham@users.noreply.github.com>
2024-02-05 20:29:09 +0100

    Update ReadMorePage.jsx

--- tp_igl_frontend/src/pages/ReadMorePage.jsx
+++ tp_igl_frontend/src/pages/ReadMorePage.jsx
@@ -1,15 +1,28 @@
 import React from 'react'
-import { NavbarUser } from '../components/NavbarUser'
+import { NavbarReadMore } from '../components/NavbarReadMore'
 import { AffichArticle } from '../components/AffichArticle'
+import { useAuth } from '../AuthContext';
+import Footer from '../components/Footer';
 import { useAuth } from '../AuthContext';
 
 
 
 export const ReadMorePage = () => {
+  const { userId } = useAuth();
+
+  React.useEffect(() => {
+    if (!userId) {
+      console.log('Navigating to /signin...');
+      window.location.href = '/signin'; 
+      console.log('Navigation executed.');
+    }
+  }, [userId]);
+
   return (
     <div>
-        <NavbarUser/>
+        <NavbarReadMore/>
         <AffichArticle/>
+        <Footer/>
     </div>
   )
 }
